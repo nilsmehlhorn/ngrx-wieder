4c9d533e8966327ddf6955a565711383d6c6bae5
nils <mail@nils-mehlhorn.de>
2021-01-24 16:44:33 +0100

    feat: improve history selectors

--- projects/ngrx-wieder/src/lib/undo-redo.selectors.ts
+++ projects/ngrx-wieder/src/lib/undo-redo.selectors.ts
@@ -1,21 +1,30 @@
 import { createSelector, Selector } from "@ngrx/store";
-import { DEFAULT_KEY, HistoryKey, UndoRedoState } from "./undo-redo.state";
+import { Segmenter } from "./config";
+import {
+  DEFAULT_KEY,
+  History,
+  HistoryKey,
+  UndoRedoState,
+} from "./undo-redo.state";
 
 export type HistoryProps = { key: HistoryKey };
 
 export const createHistorySelectors = <T, V extends UndoRedoState>(
-  selector: Selector<T, V>
+  selector: Selector<T, V>,
+  segmenter: Segmenter<V> = () => DEFAULT_KEY
 ) => {
   const selectHistory = createSelector(
     selector,
-    (state, { key }: HistoryProps = { key: DEFAULT_KEY }) =>
-      state.histories[key]
+    (
+      state,
+      { key }: HistoryProps = { key: segmenter(state) }
+    ): History | undefined => state.histories[key]
   );
   const selectCanUndo = createSelector(selectHistory, (history) => {
-    return history.undoable.length > 0;
+    return history && history.undoable.length > 0;
   });
   const selectCanRedo = createSelector(selectHistory, (history) => {
-    return history.undone.length > 0;
+    return history && history.undone.length > 0;
   });
   return {
     selectHistory,
