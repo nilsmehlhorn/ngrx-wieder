83e72c0a3e0dd6125252f21ab9d16e88c199e2e6
nils <mail@nils-mehlhorn.de>
2022-01-23 14:14:26 +0100

    ci: migrate travis to github actions

--- projects/ngrx-wieder/karma.conf.js
+++ projects/ngrx-wieder/karma.conf.js
@@ -8,17 +8,11 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngrx-wieder'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
     },
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -26,7 +20,11 @@ module.exports = function (config) {
         flags: ['--no-sandbox']
       }
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageReporter: {
+      type: 'lcov',
+      subdir: '.'
+    },
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
