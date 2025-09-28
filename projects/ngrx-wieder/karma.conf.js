6e26415ca6bc044944635a93bd6382c09f242388
Julian Pömp <jp@poemp.net>
2025-09-28 14:12:49 +0200

    build: update to angular 20 (#94)
    BREAKING CHANGE: Angular 20

--- projects/ngrx-wieder/karma.conf.js
+++ projects/ngrx-wieder/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
